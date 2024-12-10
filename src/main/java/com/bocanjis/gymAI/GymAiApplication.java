3ccc9ce27755986524b4e3b16d02f9e27a8c83f3
bocanjis <bocanjisrdjan@gmail.com>
2024-12-10 18:05:17 +0100

    initial commit

---
+++ src/main/java/com/bocanjis/gymAI/GymAiApplication.java
@@ -0,0 +1,13 @@
+package com.bocanjis.gymAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bocanjis/gymAI/GymAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bocanjis.gymAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
